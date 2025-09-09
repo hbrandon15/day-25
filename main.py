71c83903dbddb619411dfd7fe16f0f1f8b4c66aa
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2025-09-09 14:19:55 -0500

    refactor: streamline weather data processing using pandas

---
+++ main.py
@@ -0,0 +1,21 @@
+# with open("weather_data.csv") as data:
+#     weather = data.readlines()
+#     print(weather)
+#
+
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
+
+
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+print (data["temp"])
+

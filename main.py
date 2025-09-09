f72efa614b1bd7f45359efdad53e03e5cab2d753
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2025-09-09 14:33:49 -0500

    add: enhance weather data analysis with pandas methods

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+
+# # # Read the data from a file
+# # # Method 1
 # with open("weather_data.csv") as data:
 #     weather = data.readlines()
 #     print(weather)
 #
-
+# # Method 2
 # import csv
 #
 # with open("weather_data.csv") as data_file:
@@ -13,7 +16,7 @@
 #             temperatures.append(int(row[1]))
 #     print(temperatures)
 
-
+# # Method 3
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
@@ -22,5 +25,12 @@ temp_list = (data["temp"]).to_list()
 # avg_temp = round(sum(temp_list) / len(temp_list))
 # print(avg_temp)
 
+# Using pandas methods
 print (round(data["temp"].mean()))
+print (f'{data["temp"].max()} is the max temperature')
+
+# Get data in columns
+print(data["condition"])
+print(data.condition)
+
 
